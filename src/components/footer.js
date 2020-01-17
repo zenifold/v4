ba16de4c2c0ee6445dc030ef9e2aad624f7fdb16
zenifold <MaxMurphy190@yahoo.com>
2020-01-17 08:19:01 -0500

    footer update

--- src/components/footer.js
+++ src/components/footer.js
@@ -124,10 +124,10 @@ const Footer = () => {
       </StyledSocial>
       <StyledMetadata tabindex="-1">
         <StyledGitHubLink
-          href="https://github.com/bchiang7/v4"
+          href="https://github.com/zenifold/v4"
           target="_blank"
           rel="nofollow noopener noreferrer">
-          <div>Designed &amp; Built by Brittany Chiang</div>
+          <div>Designed &amp; Built by Max Murphy</div>
 
           {githubInfo.stars && githubInfo.forks && (
             <StyledGitHubInfo>
