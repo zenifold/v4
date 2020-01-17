3767df57e69f780fea45b151fcb91ac92656718b
zenifold <MaxMurphy190@yahoo.com>
2020-01-17 02:59:06 -0500

    mm update

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'MaxMurphy',
+        short_name: 'MaxMurphy',
         start_url: '/',
         background_color: config.darkNavyColor,
         theme_color: config.navyColor,
